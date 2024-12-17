b48564642cb60fa79f538c16167b7c6646bf8477
rohitkshirsagar1121 <kshirsagarrohit1111@gmail.com>
2024-12-17 17:35:14 +0530

    React using Jenkins

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>React App Jenkins</title>
+      </head>
+      <body>
+        <p>This is demo of Jenkins applciation using React. </p>
+      </body>
+    </html>
+  )
+}
+
+export default App;
